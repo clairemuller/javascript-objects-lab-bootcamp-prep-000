fb3d88518d2b8d0e426720080735e3f6ad3feed9
Claire Muller <clairermuller@gmail.com>
2018-12-23 07:09:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {};
+
+
+const updateObjectWithKeyAndValue = (object, key, value) => {
+  
+  return object;
+};
+
+const destructivelyUpdateObjectWithKeyAndValue = (object, key, value) => {
+  
+  return object;
+};
+
+const deleteFromObjectByKey = (object, key) => {
+};
+
+const destructivelyDeleteFromObjectByKey = (object, key) => {
+  
+};
+
+
