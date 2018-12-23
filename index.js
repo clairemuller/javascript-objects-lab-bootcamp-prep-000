9181b51e332ca9c2df3a5f1f4d2491465f8525ea
Claire Muller <clairermuller@gmail.com>
2018-12-23 07:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {};
 
 const updateObjectWithKeyAndValue = (object, key, value) => {
   var obj = { prop: 1 }
-  Object.assign({}, object, )
+  Object.assign({}, object, {key: value});
   return object;
 };
 
